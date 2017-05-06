0631d294c734ae8fdcb7a508e009e1ca95a0f943
24h <24h@TRUNG-PC>
2017-05-06 13:03:03 +0700

    Add project files.

---
+++ Socket-Ontap/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TCP_Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string dl;
+            byte[] data = new byte[1024 * 5];
+            //Khởi tạo socket để trao đổi dữ liệu
+            Socket server = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            IPEndPoint _ipe = new IPEndPoint(IPAddress.Any, 2017);
+
+            //gán ipe cho server
+            server.Bind(_ipe);
+            server.Listen(10);
+            // chấp nhận kết nối từ client
+            Socket client = server.Accept();
+
+            //gui chuoi ket noi toi client
+            string dataSend = "da ket noi";
+            data = Encoding.ASCII.GetBytes(dataSend);
+            client.Send(data);
+            //trao đổi dữ liệu với client
+
+            while (true)
+            {
+                data = new byte[1024 * 5];
+                int rcv = client.Receive(data);
+                dl = Encoding.ASCII.GetString(data, 0, rcv);
+                Console.WriteLine(dl);
+            }
+        }
+    }
+}
---
+++ TCP-Client/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TCP_Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string dl;
+            byte[] data = new byte[1024 * 5];
+            //Khởi tạo socket để trao đổi dữ liệu
+            Socket client = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            IPEndPoint _ipe = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 2017);
+
+            // kết nối tới server
+            client.Connect(_ipe);
+            if (client.Connected)
+            {
+                //nhan chuoi ket noi tu server
+                client.Receive(data);
+                dl = Encoding.ASCII.GetString(data);
+                Console.WriteLine(dl);
+            }
+            //trao đổi dữ liệu với server
+
+            while (true)
+            {
+                data = new byte[1024 * 5];
+                dl = Console.ReadLine();
+                data = Encoding.ASCII.GetBytes(dl);
+                client.Send(data);
+
+            }
+
+        }
+    }
+}
